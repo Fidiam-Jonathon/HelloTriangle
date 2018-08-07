6f2f32d03434ea4ab785de686c79507703e81895
Fidiam-Jonathon <fid15001@byui.edu>
2018-08-06 17:56:33 -0700

    added camera system and movement keys that affect camera position.  Still need to work out why the camera is not actually moving (even though the positions are changing)

--- Hello Window/Hello Window.cpp
+++ Hello Window/Hello Window.cpp
@@ -4,6 +4,9 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 #include <iostream>
 #include <cmath>
 #include <filesystem>
@@ -18,8 +21,16 @@ void loadTextureData(const char* path);
 void setTexture2DAttribs();
 
 // settings
-const unsigned int SCR_WIDTH = 1920;
-const unsigned int SCR_HEIGHT = 1080;
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+float alpha = 0.0;
+float FOV = 45.0f;
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
 
 int main()
 {
@@ -52,6 +63,7 @@ if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	std::cout << "Failed to initialize GLAD" << std::endl;
 	return -1;
 }
+glEnable(GL_DEPTH_TEST);
 
 /************************************
 * Shdaer init and linking
@@ -60,12 +72,72 @@ Shader shader("../Shaders/vertexShader.txt", "../shaders/fragshader.txt");
 /**********************************
 * vertices setup, VAO & VBO setup
 **********************************/
+
+/*
+
 float vert1[] = {
 	// positions         // colors		// tex coords
 	-0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // bottom left
 	0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,// bottom right
 	-0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f, 0.0f, 1.0f,  // top left
 	0.5f, 0.5f, 0.0f, 1.0f, 0.5f, 0.5f, 1.0f, 1.0f    // top right
+};
+*/
+float vert1[] = {
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
+glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
 };
 
 // indices for EBO
@@ -79,14 +151,6 @@ float vert2[] =
 	-0.5f, -0.5f, 0.0f, // bottom left
 	0.5f, -0.5f, 0.0f,  // bottom right
 	-0.5f, 0.5f, 0.0f    // top left
-};
-
-
-float texCoords[] =
-{
-	0.0f, 0.0f,
-	1.0f, 0.0f,
-	0.5f, 1.0f
 };
 
 // init VBO, VAO & EBO
@@ -105,12 +169,12 @@ glBindBuffer(GL_ARRAY_BUFFER, VBO1);
 glBufferData(GL_ARRAY_BUFFER, sizeof(vert1), vert1, GL_STATIC_DRAW);
 
 // setup and enable vertex attribs
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 glEnableVertexAttribArray(0);
-glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 glEnableVertexAttribArray(1);
-glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-glEnableVertexAttribArray(2);
+//glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+//glEnableVertexAttribArray(2);
 
 // bind and load textures
 
@@ -130,13 +194,20 @@ glBindTexture(GL_TEXTURE_2D, 0);
 glBindBuffer(GL_ARRAY_BUFFER, 0);
 glBindVertexArray(0);
 
-shader.use();
-shader.setInt("tex1", 0);
-shader.setInt("tex2", 1);
-shader.setFloat("alpha", 0.0);
-float alpha = 0.0;
-
 glm::mat4 trans(1.0f);
+
+float aspectRatio = (float)SCR_WIDTH / (float)SCR_HEIGHT;
+
+glm::mat4 view(1);
+glm::mat4 projection(1);
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+//std::cout << cameraDirection.x << cameraDirection.y << cameraDirection.z << std::endl;
+glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+//std::cout << cameraRight.x << cameraRight.y << cameraRight.z << std::endl;
+glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
 
 //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
@@ -144,27 +215,23 @@ glm::mat4 trans(1.0f);
 // -----------
 while (!glfwWindowShouldClose(window))
 {
+	
 	// input
 	// -----
-	processInput(window);
+	float currentFrame = glfwGetTime();
+	deltaTime = currentFrame - lastFrame;
+	lastFrame = currentFrame;
 
-	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-		alpha += 0.01;
-			shader.setFloat("alpha", alpha);
-	}
-	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-		alpha -= 0.01;
-		shader.setFloat("alpha", alpha);
-	}
+	processInput(window);
+	shader.use();
 	
-	trans = glm::rotate(trans, glm::radians(0.1f), glm::vec3(0.0f, 0.0f, 1.0f));
-	trans = glm::translate(trans, glm::vec3(0.005f, -0.005f, 0.0f));
-	
-	shader.setMat4("transform", trans);
-	// render
-	// ------
+	view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+	projection = glm::perspective(glm::radians(FOV), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.00f);
+	shader.setMat4("view", view);
+	shader.setMat4("projection", projection);
+		
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, TEX1);
@@ -172,8 +239,27 @@ while (!glfwWindowShouldClose(window))
 	glBindTexture(GL_TEXTURE_2D, TEX2);
 	glBindVertexArray(VAO1);
 
+	for (unsigned int i= 0; i < 10; i++) {
+
+		// calculate the model matrix for each object and pass it to shader before drawing
+		glm::mat4 model(1);
+		model = glm::translate(model, cubePositions[i]);
+		float angle = 20.0f * i;
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+		shader.setMat4("model", model);
+		shader.setInt("tex1", 0);
+		shader.setInt("tex2", 1);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
+
 	
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	//trans = glm::rotate(trans, glm::radians(0.1f), glm::vec3(0.0f, 0.0f, 1.0f));
+	//trans = glm::translate(trans, glm::vec3(0.005f, -0.005f, 0.0f));
+	
+	//shader.setMat4("transform", trans);
+	// render
+	// ------
+	//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 
 
@@ -192,10 +278,33 @@ return 0;
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
+void processInput(GLFWwindow* window)
 {
+
+	float cameraSpeed = 10.0f * deltaTime; // adjust accordingly
+
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		cameraPos -= cameraSpeed * cameraFront;
+		std::cout << cameraPos.x << " " << cameraPos.y << " " << cameraPos.z << std::endl;
+	}
+		
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		cameraPos += cameraSpeed * cameraFront;
+		std::cout << cameraPos.x << " " << cameraPos.y << " " << cameraPos.z << std::endl;
+	}
+		
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+		std::cout << cameraPos.x << " " << cameraPos.y << " " << cameraPos.z << std::endl;
+	}
+		
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+		std::cout << cameraPos.x << " " << cameraPos.y << " " << cameraPos.z << std::endl;
+	}
+		
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
